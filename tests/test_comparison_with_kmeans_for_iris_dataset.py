2988e2718af199f6dccf2d7154dac2332b39769c
Gabriel Carvalho Santana <74666057+obb199@users.noreply.github.com>
2022-09-17 20:05:26 -0300

    Add files via upload

---
+++ tests/test_comparison_with_kmeans_for_iris_dataset.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+from cluster import *
+from utils import argmin
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.cluster import KMeans
+
+if __name__ == '__main__':
+    dataset = load_iris()
+    classes = len(dataset.target_names)
+
+    X_train, X_test, y_train, y_test = train_test_split(dataset.data,
+                                                        dataset.target,
+                                                        test_size=0.3,
+                                                        shuffle=True,
+                                                        random_state=42)
+
+    cluster_points = cluster_generator(X_train, 3)
+    k_means_model = KMeans(n_clusters=2, random_state=0).fit(X_train)
+
+    matrix_confusion = [[0 for _ in range(classes)] for _ in range(classes)]
+    matrix_confusion_kmeans = [[0 for _ in range(classes)] for _ in range(classes)]
+
+    for x, y in zip(X_test, y_test):
+        distances_from_clusters = distance_from_centroids(x, cluster_points)
+        predict = argmin(distance_from_centroids(x, cluster_points))
+        matrix_confusion[predict-1][y-1] += 1
+
+        predict_kmeans = k_means_model.predict([x])
+        matrix_confusion_kmeans[predict-1][y-1] += 1
+
+    for i, target in enumerate(dataset.target_names):
+        plt.subplot(2, 3, i+1)
+        plt.title(f"Results when target is {i}")
+        for j, res in enumerate(matrix_confusion[i]):
+            plt.bar(f"{dataset.target_names[j]}", res)
+
+    for i, target in enumerate(dataset.target_names):
+        plt.subplot(2, 3, i+4)
+        plt.title(f"Results with kmeans when target is {i}")
+        for j, res in enumerate(matrix_confusion_kmeans[i]):
+            plt.bar(f"{dataset.target_names[j]}", res)
+
+    print(matrix_confusion)
+    print(matrix_confusion_kmeans)
+    plt.savefig("iris_dataset_comparison_with_kmeans_tests")
+    plt.show()
