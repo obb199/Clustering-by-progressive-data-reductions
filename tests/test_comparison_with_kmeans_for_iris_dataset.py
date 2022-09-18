50071e7822c3095909468efb701d5e942f6bd9a2
Gabriel Carvalho Santana <74666057+obb199@users.noreply.github.com>
2022-09-18 01:21:54 -0300

    Update test_comparison_with_kmeans_for_iris_dataset.py

--- tests/test_comparison_with_kmeans_for_iris_dataset.py
+++ tests/test_comparison_with_kmeans_for_iris_dataset.py
@@ -1,9 +1,8 @@
 import matplotlib.pyplot as plt
-from cluster import *
-from utils import argmin
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.cluster import KMeans
+from cluster import Cluster
 
 if __name__ == '__main__':
     dataset = load_iris()
@@ -15,19 +14,22 @@ if __name__ == '__main__':
                                                         shuffle=True,
                                                         random_state=42)
 
-    cluster_points = cluster_generator(X_train, 3)
-    k_means_model = KMeans(n_clusters=2, random_state=0).fit(X_train)
+    cluster_points = Cluster(3)
+    cluster_points.fit(X_train)
+    k_means_model = KMeans(n_clusters=3, random_state=0)
+    k_means_model.fit(X_train)
 
     matrix_confusion = [[0 for _ in range(classes)] for _ in range(classes)]
     matrix_confusion_kmeans = [[0 for _ in range(classes)] for _ in range(classes)]
 
-    for x, y in zip(X_test, y_test):
-        distances_from_clusters = distance_from_centroids(x, cluster_points)
-        predict = argmin(distance_from_centroids(x, cluster_points))
-        matrix_confusion[predict-1][y-1] += 1
+    predict = cluster_points.predict(X_test)
+    predict_kmeans = k_means_model.predict(X_test)
 
-        predict_kmeans = k_means_model.predict([x])
-        matrix_confusion_kmeans[predict-1][y-1] += 1
+    i = -1
+    for x, y in zip(X_test, y_test):
+        i += 1
+        matrix_confusion[predict[i]-1][y-1] += 1
+        matrix_confusion_kmeans[predict_kmeans[i]-1][y-1] += 1
 
     plt.figure(figsize=(8, 8))
     for i, target in enumerate(dataset.target_names):
