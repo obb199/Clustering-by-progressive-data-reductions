ea9c1ac3422a146eabd5d37824b656b88c8e3fb7
Gabriel Carvalho Santana <74666057+obb199@users.noreply.github.com>
2022-09-18 01:45:57 -0300

    Update test_for_circles.py

--- tests/test_for_circles.py
+++ tests/test_for_circles.py
@@ -6,19 +6,19 @@ if __name__ == '__main__':
     points = []
 
     # circles
-    points1 = circular_distribution_generator(r=2,
+    points1 = circular_distribution_generator(radius=2,
                                               concentration_rate=1.5,
                                               n_points=100,
                                               center_position_x=0,
                                               center_position_y=3)
 
-    points2 = circular_distribution_generator(r=2,
+    points2 = circular_distribution_generator(radius=2,
                                               concentration_rate=1,
                                               n_points=100,
                                               center_position_x=5,
                                               center_position_y=8)
 
-    points3 = circular_distribution_generator(r=2,
+    points3 = circular_distribution_generator(radius=2,
                                               concentration_rate=0.5,
                                               n_points=100,
                                               center_position_x=3,
