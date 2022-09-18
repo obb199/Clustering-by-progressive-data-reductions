21d53aa15b24f2f080d36921e0df533dc2047def
Gabriel Carvalho Santana <74666057+obb199@users.noreply.github.com>
2022-09-18 01:11:37 -0300

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,9 @@
-from cluster import *
+from cluster import Cluster
 import matplotlib.pyplot as plt
 
 
 def algorithm_test(clusters_matrix, points, name_output_figure):
-    plt.figure(figsize=(30, 50))
+    plt.figure(figsize=(15, 20))
     colors = ['blue', 'orange', 'pink', 'green', 'purple', 'black']
 
     j = 0
@@ -13,13 +13,14 @@ def algorithm_test(clusters_matrix, points, name_output_figure):
             plt.subplot(len(clusters_matrix), len(clusters_matrix[0]), j)
             plt.title(f'Number of clusters:' + str(number_of_clusters))
 
-            clusters = cluster_generator(points[point_idx], number_of_clusters)
+            clusters = Cluster(number_of_clusters)
+            clusters.fit(points[point_idx])
 
             for p in points[point_idx]:
-                cluster_color = argmin(distance_from_centroids(p, clusters))
+                cluster_color = clusters.predict(p)
                 plt.scatter(p[0], p[1], color=colors[cluster_color], alpha=0.6)
 
-            for c in clusters:
+            for c in clusters.centroids_positions:
                 plt.scatter(c[0], c[1], color='red')
 
     plt.savefig(name_output_figure)
